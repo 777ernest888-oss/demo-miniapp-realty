94cef1051ad79cc9e6f0812a0ec934e0ff8cbe00
777ernest888-oss <777ernest888@gmail.com>
2026-05-21 14:13:30 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-﻿const APP_CONFIG = {
+const APP_CONFIG = {
   // 🎨 БРЕНД (МЕНЯЙТЕ ТОЛЬКО ЭТИ 4 СТРОКИ ПОД КЛИЕНТА)
   brand: {
     name: "Название Агентства",
@@ -6,6 +6,6 @@
     logo: "https://via.placeholder.com/150?text=LOGO",
     contactLink: "https://t.me/your_username"
   },
-  // 📊 ССЫЛКА НА ДАННЫЕ (Замените ВАШ_ID_ТАБЛИЦЫ)
+  // 📊 ССЫЛКА НА ДАННЫЕ (Замените ВАШ_ID_ТАБЛИЦЫ на реальный ID)
   sheetUrl: "https://docs.google.com/spreadsheets/d/1z7d0dv5o_JKVfIQgn38rHoAywZazadRSvJQFeQgV3eM/export?format=csv&gid=0"
 };
